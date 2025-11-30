414aa3469a0fbd56a8e86f80fb214de1dff9360e
Glenardd <tolentinojanglenard5@gmail.com>
2025-12-01 00:01:13 +0800

    fix: change ipcRenderer.send to ipcRenderer.invoke for temperature and add location handling

--- preload.js
+++ preload.js
@@ -4,5 +4,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('weatherStore', {
     api: (data) => ipcRenderer.invoke('get-api', data),
     weather_code: () =>  ipcRenderer.invoke('get-weather-codes'),
-    temp: (temp) => ipcRenderer.send('temp', temp)
+    temp: (temp) => ipcRenderer.invoke('temp', temp),
+    location: () => ipcRenderer.invoke('locate')
 });
