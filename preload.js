448d4c6c54c413b82868e2c4383765704e873dc1
Glenardd <tolentinojanglenard5@gmail.com>
2025-12-01 02:56:49 +0800

    fix: update location function to accept data parameter for ipcRenderer.invoke

--- preload.js
+++ preload.js
@@ -5,5 +5,5 @@ contextBridge.exposeInMainWorld('weatherStore', {
     api: (data) => ipcRenderer.invoke('get-api', data),
     weather_code: () =>  ipcRenderer.invoke('get-weather-codes'),
     temp: (temp) => ipcRenderer.invoke('temp', temp),
-    location: () => ipcRenderer.invoke('locate')
+    location: (data) => ipcRenderer.invoke('locate',data),
 });
